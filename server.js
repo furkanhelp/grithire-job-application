28deffeaae162a6663da848245c4f10f7c529a4d
Furkan <frknyrdm0@gmail.com>
2025-10-25 14:24:42 +0300

    v0.8.3 front-end update

--- server.js
+++ server.js
@@ -54,7 +54,10 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/v1/auth/google/callback",
+      callbackURL:
+        process.env.NODE_ENV === "production"
+          ? "https://grithire-job-application.onrender.com/api/v1/auth/google/callback"
+          : "http://localhost:5000/api/v1/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
